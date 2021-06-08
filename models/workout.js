bcc85b6c625fcf8e9924deac4b27b6aab933f7f5
Elise Daly <elise.samantha.daly@gmail.com>
2021-06-08 15:31:40 -0400

    Adding routes

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true
+      },
+      type: {
+        type: String,
+        trim: true
+      },
+      weight: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      duration: {
+        type: Number,
+        default: 0
+      },
+      distance: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
